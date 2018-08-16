dcd1643977c5526ed8982ea548505c80ee98112f
gregwhitaker <gwhitake@gmail.com>
2018-08-16 12:56:30 -0700

    Added travis file

--- buildtravis.sh
+++ buildtravis.sh
@@ -8,18 +8,14 @@ echo -e "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  cd rsocket-rpc-java
   ./gradlew clean build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  cd rsocket-rpc-java
   ./gradlew clean build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  cd rsocket-rpc-java
   ./gradlew -PreleaseType=release -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" clean build bintrayUpload --stacktrace
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  cd rsocket-rpc-java
   ./gradlew clean build
 fi
