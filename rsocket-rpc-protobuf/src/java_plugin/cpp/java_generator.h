b07b541b18dad025961bf75ee3d0fbe67da2df36
Ryland Degnan <ryland@netifi.com>
2018-09-06 14:08:33 -0700

    Prefix artifacts with rsocket-rpc

---
+++ rsocket-rpc-protobuf/src/java_plugin/cpp/java_generator.h
@@ -0,0 +1,71 @@
+#ifndef RSOCKET_RPC_COMPILER_JAVA_GENERATOR_H_
+#define RSOCKET_RPC_COMPILER_JAVA_GENERATOR_H_
+
+#include <stdlib.h>  // for abort()
+#include <iostream>
+#include <string>
+
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/descriptor.h>
+
+class LogHelper {
+  std::ostream* os;
+
+ public:
+  LogHelper(std::ostream* os) : os(os) {}
+  ~LogHelper() {
+    *os << std::endl;
+    ::abort();
+  }
+  std::ostream& get_os() {
+    return *os;
+  }
+};
+
+// Abort the program after logging the mesage if the given condition is not
+// true. Otherwise, do nothing.
+#define RSOCKET_RPC_CODEGEN_CHECK(x) !(x) && LogHelper(&std::cerr).get_os() \
+                             << "CHECK FAILED: " << __FILE__ << ":" \
+                             << __LINE__ << ": "
+
+// Abort the program after logging the mesage.
+#define RSOCKET_RPC_CODEGEN_FAIL RSOCKET_RPC_CODEGEN_CHECK(false)
+
+using namespace std;
+
+namespace java_rsocket_rpc_generator {
+
+enum ProtoFlavor {
+  NORMAL, LITE
+};
+
+// Returns the package name of the RSocket RPC services defined in the given file.
+string ServiceJavaPackage(const google::protobuf::FileDescriptor* file);
+
+// Returns the name of the client class for the given service.
+string ClientClassName(const google::protobuf::ServiceDescriptor* service);
+
+// Returns the name of the client class for the given service.
+string ServerClassName(const google::protobuf::ServiceDescriptor* service);
+
+// Writes the generated interface into the given ZeroCopyOutputStream
+void GenerateInterface(const google::protobuf::ServiceDescriptor* service,
+                       google::protobuf::io::ZeroCopyOutputStream* out,
+                       ProtoFlavor flavor,
+                       bool disable_version);
+
+// Writes the generated client into the given ZeroCopyOutputStream
+void GenerateClient(const google::protobuf::ServiceDescriptor* service,
+                    google::protobuf::io::ZeroCopyOutputStream* out,
+                    ProtoFlavor flavor,
+                    bool disable_version);
+
+// Writes the generated server into the given ZeroCopyOutputStream
+void GenerateServer(const google::protobuf::ServiceDescriptor* service,
+                    google::protobuf::io::ZeroCopyOutputStream* out,
+                    ProtoFlavor flavor,
+                    bool disable_version);
+
+}  // namespace java_rsocket_rpc_generator
+
+#endif  // RSOCKET_RPC_COMPILER_JAVA_GENERATOR_H_
---
+++ rsocket-rpc-protobuf/src/java_plugin/cpp/java_plugin.cpp
@@ -0,0 +1,118 @@
+#include <memory>
+
+#include "java_generator.h"
+#include "blocking_java_generator.h"
+#include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <iostream>
+
+static string JavaPackageToDir(const string& package_name) {
+  string package_dir = package_name;
+  for (size_t i = 0; i < package_dir.size(); ++i) {
+    if (package_dir[i] == '.') {
+      package_dir[i] = '/';
+    }
+  }
+  if (!package_dir.empty()) package_dir += "/";
+  return package_dir;
+}
+
+class JavaRSocketRpcGenerator : public google::protobuf::compiler::CodeGenerator {
+ public:
+  JavaRSocketRpcGenerator() {}
+  virtual ~JavaRSocketRpcGenerator() {}
+
+  virtual bool Generate(const google::protobuf::FileDescriptor* file,
+                        const string& parameter,
+                        google::protobuf::compiler::GeneratorContext* context,
+                        string* error) const {
+    return reactor(file, parameter, context, error) && blocking(file, parameter, context, error);
+   // return reactor(file, parameter, context, error);
+  }
+
+  virtual bool reactor(const google::protobuf::FileDescriptor* file,
+                          const string& parameter,
+                          google::protobuf::compiler::GeneratorContext* context,
+                          string* error) const {
+    std::vector<std::pair<string, string> > options;
+    google::protobuf::compiler::ParseGeneratorParameter(parameter, &options);
+
+    java_rsocket_rpc_generator::ProtoFlavor flavor =
+     java_rsocket_rpc_generator::ProtoFlavor::NORMAL;
+
+    bool disable_version = false;
+    for (size_t i = 0; i < options.size(); i++) {
+        if (options[i].first == "lite") {
+            flavor = java_rsocket_rpc_generator::ProtoFlavor::LITE;
+        } else if (options[i].first == "noversion") {
+            disable_version = true;
+        }
+    }
+
+    string package_name = java_rsocket_rpc_generator::ServiceJavaPackage(file);
+    string package_filename = JavaPackageToDir(package_name);
+    for (int i = 0; i < file->service_count(); ++i) {
+        const google::protobuf::ServiceDescriptor* service = file->service(i);
+
+        string interface_filename = package_filename + service->name() + ".java";
+        std::unique_ptr<google::protobuf::io::ZeroCopyOutputStream> interface_file(context->Open(interface_filename));
+        java_rsocket_rpc_generator::GenerateInterface(service, interface_file.get(), flavor, disable_version);
+
+        string client_filename = package_filename + java_rsocket_rpc_generator::ClientClassName(service) + ".java";
+        std::unique_ptr<google::protobuf::io::ZeroCopyOutputStream> client_file(context->Open(client_filename));
+        java_rsocket_rpc_generator::GenerateClient(service, client_file.get(), flavor, disable_version);
+
+        string server_filename = package_filename + java_rsocket_rpc_generator::ServerClassName(service) + ".java";
+        std::unique_ptr<google::protobuf::io::ZeroCopyOutputStream> server_file(context->Open(server_filename));
+        java_rsocket_rpc_generator::GenerateServer(service, server_file.get(), flavor, disable_version);
+    }
+    return true;
+  }
+
+  virtual bool blocking(const google::protobuf::FileDescriptor* file,
+                          const string& parameter,
+                          google::protobuf::compiler::GeneratorContext* context,
+                          string* error) const {
+    std::vector<std::pair<string, string> > options;
+    google::protobuf::compiler::ParseGeneratorParameter(parameter, &options);
+
+    blocking_java_rsocket_rpc_generator::ProtoFlavor flavor =
+    blocking_java_rsocket_rpc_generator::ProtoFlavor::NORMAL;
+
+    bool disable_version = false;
+    for (size_t i = 0; i < options.size(); i++) {
+        if (options[i].first == "lite") {
+            flavor = blocking_java_rsocket_rpc_generator::ProtoFlavor::LITE;
+        } else if (options[i].first == "noversion") {
+            disable_version = true;
+        }
+    }
+
+    string package_name = blocking_java_rsocket_rpc_generator::ServiceJavaPackage(file);
+    string package_filename = JavaPackageToDir(package_name);
+    for (int i = 0; i < file->service_count(); ++i) {
+        const google::protobuf::ServiceDescriptor* service = file->service(i);
+
+        string interface_filename = package_filename + "Blocking" + service->name() + ".java";
+        std::unique_ptr<google::protobuf::io::ZeroCopyOutputStream> interface_file(context->Open(interface_filename));
+        blocking_java_rsocket_rpc_generator::GenerateInterface(service, interface_file.get(), flavor, disable_version);
+
+        string client_filename = package_filename + "Blocking" + java_rsocket_rpc_generator::ClientClassName(service) + ".java";
+        std::unique_ptr<google::protobuf::io::ZeroCopyOutputStream> client_file(context->Open(client_filename));
+        blocking_java_rsocket_rpc_generator::GenerateClient(service, client_file.get(), flavor, disable_version);
+
+        string server_filename = package_filename + "Blocking" + java_rsocket_rpc_generator::ServerClassName(service) + ".java";
+        std::unique_ptr<google::protobuf::io::ZeroCopyOutputStream> server_file(context->Open(server_filename));
+        blocking_java_rsocket_rpc_generator::GenerateServer(service, server_file.get(), flavor, disable_version);
+    }
+    return true;
+  }
+
+};
+
+int main(int argc, char* argv[]) {
+  JavaRSocketRpcGenerator generator;
+  return google::protobuf::compiler::PluginMain(argc, argv, &generator);
+}
