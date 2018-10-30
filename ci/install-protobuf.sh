e1297a6b266826f54c2140341710da0e296753ef
Oleh Dokuka <shadowgun@i.ua>
2018-10-30 16:19:40 +0200

    test caching

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 # check to see if protobuf folder is empty
-if [ ! -d "$HOME/rsocket/rsocket-rpc-java/protobuf-3.6.1" ]; then
+if [ ! -d "$HOME/build/rsocket/rsocket-rpc-java/protobuf-3.6.1" ]; then
   wget https://github.com/google/protobuf/releases/download/v3.6.1/protobuf-cpp-3.6.1.tar.gz
   tar -xzvf protobuf-cpp-3.6.1.tar.gz
   pushd protobuf-3.6.1 && ./configure --disable-shared && make && sudo make install && popd
