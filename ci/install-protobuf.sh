67e73dd796df3ceba1d30addf1f7c07ff1f62f3e
Oleh Dokuka <shadowgun@i.ua>
2018-10-30 15:39:27 +0200

    test cacheing

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 # check to see if protobuf folder is empty
-if [ ! -d "google/protobuf/lib" ]; then
+if [ ! -d "$HOME/google/protobuf/lib" ]; then
   wget https://github.com/google/protobuf/releases/download/v3.6.1/protobuf-cpp-3.6.1.tar.gz
   tar -xzvf protobuf-cpp-3.6.1.tar.gz
   pushd protobuf-3.6.1 && ./configure --disable-shared && make && sudo make install && popd
