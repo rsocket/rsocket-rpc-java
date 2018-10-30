d4a610f8284112dbf1338af5984fab00892d14e1
Oleh Dokuka <shadowgun@i.ua>
2018-10-30 09:55:43 +0200

    fix protobuf path

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 # check to see if protobuf folder is empty
-if [ ! -d "$HOME/protobuf/lib" ]; then
+if [ ! -d "$HOME/google/protobuf/lib" ]; then
   wget https://github.com/google/protobuf/releases/download/v3.6.1/protobuf-cpp-3.6.1.tar.gz
   tar -xzvf protobuf-cpp-3.6.1.tar.gz
-  pushd protobuf-3.6.1 && ./configure --prefix=$HOME/protobuf --disable-shared && make && sudo make install && popd
+  pushd protobuf-3.6.1 && ./configure --disable-shared && make && sudo make install && popd
 else
   echo "Using cached directory."
 fi
