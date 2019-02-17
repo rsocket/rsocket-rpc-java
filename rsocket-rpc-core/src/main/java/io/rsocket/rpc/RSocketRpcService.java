25c369539c2d4b0454f798e42eb4c977f617876c
Robert Roeser <rroeserr@gmail.com>
2019-02-17 13:45:08 -0800

    leaves method signatures the sames 0.2.12 and earlier so that it doesnt cause override errors when upgrading

--- rsocket-rpc-core/src/main/java/io/rsocket/rpc/RSocketRpcService.java
+++ rsocket-rpc-core/src/main/java/io/rsocket/rpc/RSocketRpcService.java
@@ -1,7 +1,16 @@
 package io.rsocket.rpc;
 
+import io.rsocket.Payload;
 import io.rsocket.ResponderRSocket;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
 
 public interface RSocketRpcService extends ResponderRSocket {
   String getService();
+
+  Flux<Payload> requestChannel(Payload payload, Flux<Payload> publisher);
+
+  default Flux<Payload> requestChannel(Payload payload, Publisher<Payload> payloads) {
+    return requestChannel(payload, Flux.from(payloads));
+  }
 }
