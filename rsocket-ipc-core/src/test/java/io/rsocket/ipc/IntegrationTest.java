861432c63e34d3931ea9a0c1119080a85854c1a9
reggie pierce <reggie.pierce@iplasso.com>
2020-05-08 17:39:52 -0400

    testing add

--- rsocket-ipc-core/src/test/java/io/rsocket/ipc/IntegrationTest.java
+++ rsocket-ipc-core/src/test/java/io/rsocket/ipc/IntegrationTest.java
@@ -18,113 +18,128 @@ package io.rsocket.ipc;
 import io.netty.buffer.ByteBufAllocator;
 import io.rsocket.RSocket;
 import io.rsocket.RSocketFactory;
+import io.rsocket.SocketAcceptor;
+import io.rsocket.core.RSocketConnector;
+import io.rsocket.core.RSocketServer;
 import io.rsocket.ipc.decoders.CompositeMetadataDecoder;
+import io.rsocket.ipc.decoders.MetadataDecoderLFP;
 import io.rsocket.ipc.encoders.DefaultMetadataEncoder;
+import io.rsocket.ipc.encoders.MetadataEncoderLFP;
 import io.rsocket.ipc.marshallers.Primitives;
 import io.rsocket.ipc.marshallers.Strings;
+import io.rsocket.ipc.mimetype.MimeTypes;
 import io.rsocket.transport.local.LocalClientTransport;
 import io.rsocket.transport.local.LocalServerTransport;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import io.rsocket.transport.netty.server.TcpServerTransport;
+
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Level;
+
 import org.junit.Assert;
 import org.junit.Test;
+
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class IntegrationTest {
-  @Test
-  public void test() {
-    RequestHandlingRSocket requestHandler =
-        new RequestHandlingRSocket(new CompositeMetadataDecoder());
+	@Test
+	public void test() {
+		MetadataDecoderLFP decoder = new MetadataDecoderLFP();
+		RequestHandlingRSocket requestHandler = new RequestHandlingRSocket(decoder);
+		{// start server
+			SocketAcceptor socketAcceptor = (setup, client) -> Mono.just(requestHandler);
+			RSocketServer.create(socketAcceptor).interceptors(ir -> {
+			}).errorConsumer(t -> {
+				java.util.logging.Logger.getLogger("[server]").log(Level.SEVERE, "uncaught error", t);
+			}).bind(TcpServerTransport.create("localhost", 7000)).block();
+		}
+		MetadataEncoderLFP encoder = new MetadataEncoderLFP();
+		RSocket rsocket;
+		{// start client
+			rsocket = RSocketConnector.create().connect(TcpClientTransport.create("localhost", 7000)).block();
+		}
+		AtomicBoolean ff = new AtomicBoolean();
 
-    RSocketFactory.receive()
-        .acceptor((setup, sendingSocket) -> Mono.just(requestHandler))
-        .transport(LocalServerTransport.create("test-local-server"))
-        .start()
-        .block();
+		IPCRSocket service = Server.service("HelloService").noMeterRegistry().noTracer().marshall(Strings.marshaller())
+				.unmarshall(Strings.unmarshaller()).requestResponse("hello", (s, byteBuf) -> Mono.just("Hello -> " + s))
+				.requestResponse("goodbye", (s, byteBuf) -> Mono.just("Goodbye -> " + s))
+				.requestResponse("count", Primitives.intMarshaller(),
+						(charSequence, byteBuf) -> Mono.just(charSequence.length()))
+				.requestResponse("increment", Primitives.intUnmarshaller(), Primitives.intMarshaller(),
+						(integer, byteBuf) -> Mono.just(integer + 1))
+				.requestStream("helloStream", (s, byteBuf) -> Flux.range(1, 10).map(i -> i + " - Hello -> " + s))
+				.requestStream("toString", Primitives.longUnmarshaller(),
+						(aLong, byteBuf) -> Flux.just(String.valueOf(aLong)))
+				.fireAndForget("ff", (s, byteBuf) -> {
+					ff.set(true);
+					return Mono.empty();
+				}).requestChannel("helloChannel", (s, publisher, byteBuf) -> Flux.just("Hello -> " + s)).toIPCRSocket();
 
-    RSocket rsocket =
-        RSocketFactory.connect()
-            .transport(LocalClientTransport.create("test-local-server"))
-            .start()
-            .block();
+		requestHandler.withEndpoint(service);
 
-    AtomicBoolean ff = new AtomicBoolean();
+		Client<CharSequence, String> helloService = Client.service("HelloService").rsocket(rsocket)
+				.customMetadataEncoder(new DefaultMetadataEncoder(ByteBufAllocator.DEFAULT)).noMeterRegistry()
+				.noTracer().marshall(Strings.marshaller()).unmarshall(Strings.unmarshaller());
 
-    IPCRSocket service =
-        Server.service("HelloService")
-            .noMeterRegistry()
-            .noTracer()
-            .marshall(Strings.marshaller())
-            .unmarshall(Strings.unmarshaller())
-            .requestResponse("hello", (s, byteBuf) -> Mono.just("Hello -> " + s))
-            .requestResponse("goodbye", (s, byteBuf) -> Mono.just("Goodbye -> " + s))
-            .requestResponse(
-                "count",
-                Primitives.intMarshaller(),
-                (charSequence, byteBuf) -> Mono.just(charSequence.length()))
-            .requestResponse(
-                "increment",
-                Primitives.intUnmarshaller(),
-                Primitives.intMarshaller(),
-                (integer, byteBuf) -> Mono.just(integer + 1))
-            .requestStream(
-                "helloStream", (s, byteBuf) -> Flux.range(1, 10).map(i -> i + " - Hello -> " + s))
-            .requestStream(
-                "toString",
-                Primitives.longUnmarshaller(),
-                (aLong, byteBuf) -> Flux.just(String.valueOf(aLong)))
-            .fireAndForget(
-                "ff",
-                (s, byteBuf) -> {
-                  ff.set(true);
-                  return Mono.empty();
-                })
-            .requestChannel("helloChannel", (s, publisher, byteBuf) -> Flux.just("Hello -> " + s))
-            .toIPCRSocket();
+		String r1 = helloService.requestResponse("hello").apply("Alice").block();
+		Assert.assertEquals("Hello -> Alice", r1);
 
-    requestHandler.withEndpoint(service);
+		String r2 = helloService.requestResponse("goodbye").apply("Bob").block();
+		Assert.assertEquals("Goodbye -> Bob", r2);
 
-    Client<CharSequence, String> helloService =
-        Client.service("HelloService")
-            .rsocket(rsocket)
-            .customMetadataEncoder(new DefaultMetadataEncoder(ByteBufAllocator.DEFAULT))
-            .noMeterRegistry()
-            .noTracer()
-            .marshall(Strings.marshaller())
-            .unmarshall(Strings.unmarshaller());
+		StepVerifier.create(helloService.requestStream("helloStream").apply("Carol")).expectNextCount(10)
+				.expectComplete().verify();
 
-    String r1 = helloService.requestResponse("hello").apply("Alice").block();
-    Assert.assertEquals("Hello -> Alice", r1);
+		helloService.fireAndForget("ff").apply("boom").block();
+		Assert.assertTrue(ff.get());
 
-    String r2 = helloService.requestResponse("goodbye").apply("Bob").block();
-    Assert.assertEquals("Goodbye -> Bob", r2);
+		String r3 = helloService.requestChannel("helloChannel").apply(Mono.just("Eve")).blockLast();
+		Assert.assertEquals("Hello -> Eve", r3);
 
-    StepVerifier.create(helloService.requestStream("helloStream").apply("Carol"))
-        .expectNextCount(10)
-        .expectComplete()
-        .verify();
+		int count = helloService.requestResponse("count", Primitives.intUnmarshaller()).apply("hello").block();
+		Assert.assertEquals(5, count);
 
-    helloService.fireAndForget("ff").apply("boom").block();
-    Assert.assertTrue(ff.get());
+		long l = System.currentTimeMillis();
+		String toString = helloService.requestStream("toString", Primitives.longMarshaller()).apply(l).blockLast();
+		Assert.assertEquals(String.valueOf(l), toString);
 
-    String r3 = helloService.requestChannel("helloChannel").apply(Mono.just("Eve")).blockLast();
-    Assert.assertEquals("Hello -> Eve", r3);
-
-    int count =
-        helloService.requestResponse("count", Primitives.intUnmarshaller()).apply("hello").block();
-    Assert.assertEquals(5, count);
-
-    long l = System.currentTimeMillis();
-    String toString =
-        helloService.requestStream("toString", Primitives.longMarshaller()).apply(l).blockLast();
-    Assert.assertEquals(String.valueOf(l), toString);
-
-    Integer increment =
-        helloService
-            .requestResponse("increment", Primitives.intMarshaller(), Primitives.intUnmarshaller())
-            .apply(1)
-            .block();
-    Assert.assertEquals(2, increment.intValue());
-  }
+		Integer increment = helloService
+				.requestResponse("increment", Primitives.intMarshaller(), Primitives.intUnmarshaller()).apply(1)
+				.block();
+		Assert.assertEquals(2, increment.intValue());
+		Disposable encoderPasswordDisposable = null;
+		for (int i = 0; i < 3; i++) {
+			if (i == 1) {
+				encoderPasswordDisposable = encoder.addInterceptor(writer -> {
+					// writer.writeString(MimeTypes.create("password"), "AAAAAAAAAAAA");
+					writer.writeString(MimeTypes.create("password"), "thisIsACoolPassWord!");
+				});
+				decoder.addInterceptor(reader -> {
+					boolean match = reader.containsStringSecure(MimeTypes.create("password"), "thisIsACoolPassWord!");
+					if (!match)
+						throw new IllegalArgumentException("not authorized");
+				});
+			}
+			boolean shouldFail;
+			if (i == 2) {
+				shouldFail = true;
+				encoderPasswordDisposable.dispose();
+			} else
+				shouldFail = false;
+			boolean failed;
+			try {
+				Integer incrementMetadata = helloService
+						.requestResponse("increment", Primitives.intMarshaller(), Primitives.intUnmarshaller()).apply(1)
+						.block();
+				Assert.assertEquals(2, incrementMetadata.intValue());
+				failed = false;
+			} catch (Exception e) {
+				failed = true;
+			}
+			Assert.assertEquals(failed, shouldFail);
+		}
+	}
 }
